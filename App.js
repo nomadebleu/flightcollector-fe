2857f6183172e03488b7b1b1888a9a072a0f06ba
GuillaumeeP <guillaume.pioch26@gmail.com>
2024-02-05 20:17:04 +0100

    Test branche

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from './screens/Home'
+import Galery from './screens/Galery'
+import Reduction from './screens/Reductions'
+import SignUp from './screens'
+import Profil from './screens/Profil';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Galery" component={Galery} />
+        <Tab.Screen name="Reduction" component={Reduction} />
+        <Tab.Screen name="Profil" component={Profil} />
+      </Tab.Navigator>
+  </NavigationContainer>
   );
 }
 
