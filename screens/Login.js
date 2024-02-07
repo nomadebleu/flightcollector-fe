fec804c311a85fb7171f0651ee6cccb40fc2f586
Anthony <anthonyiezzi64@gmail.com>
2024-02-07 19:15:18 +0100

    ras

--- screens/Login.js
+++ screens/Login.js
@@ -156,7 +156,7 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     backgroundColor: "#FFFFFF",
     borderColor: "#80C9FF",
-    borderWidth: "2px solid",
+    borderWidth:2,
   },
   //Forgotten
   forgotten: {
