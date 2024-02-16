d676bf31cc52584a87937e403be351b7605bf9fa
Vanessa <vane.giraldo14@gmail.com>
2024-02-16 22:05:25 +0100

    Flight onglet

--- components/MyPlaneScreen/Flight.js
+++ components/MyPlaneScreen/Flight.js
@@ -17,7 +17,18 @@ export default function Flight() {
             longitudeDelta: 0.01,
           }}
         />
-        <Text>Flight</Text>
+        <View style={styles.info}>
+          <View style={styles.dep}>
+            <Text style={styles.title}>DEPARTURE</Text>
+            <Text style={styles.item}>Scheduled 10:00AM</Text>
+            <Text style={styles.item}>Actual 10:20AM</Text>
+          </View>
+          <View style={styles.arr}>
+            <Text style={styles.title}>ARRIVAL</Text>
+            <Text style={styles.item}>Scheduled 19:40PM</Text>
+            <Text style={styles.item}>Estimated 20:00PM</Text>
+          </View>
+        </View>
       </View>
     </View>
   );
@@ -27,5 +38,26 @@ const styles = StyleSheet.create({
   onglet: {
     backgroundColor: "#75bbf4",
     flex: 1,
+  },
+  title: {
+    fontFamily: "Cabin-Bold",
+    color: "#002C82",
+    paddingLeft: 20,
+    paddingTop: 10,
+  },
+  item: {
+    padding: 3,
+    paddingLeft: 10,
+    color: "#002C82",
+    fontFamily: "Cabin-Regular",
+    justifyContent: "center",
+    width: "100%",
+    margin: 2,
+    color: "e1e5f2",
+  },
+  info: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 15,
   },
 });
