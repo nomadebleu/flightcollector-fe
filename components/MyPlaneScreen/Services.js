ad91a2e2a6635dec562502a43568375bbb0817df
Anthony <anthonyiezzi64@gmail.com>
2024-02-19 11:12:23 +0100

    services ok

--- components/MyPlaneScreen/Services.js
+++ components/MyPlaneScreen/Services.js
@@ -34,7 +34,7 @@ export default function Services() {
   return (
     <View style={styles.onglet}>
       <View style={styles.otherService}>
-        <Text style={styles.title}>YOUR SEAT</Text>
+      <Text style={[styles.title2, {marginRight: 11}]}>YOUR SEAT</Text>
         <TextInput
           style={styles.input}
           value={meals}
@@ -43,7 +43,7 @@ export default function Services() {
         />
       </View>
       <View style={styles.otherService}>
-        <Text style={styles.title}>YOUR MEALS</Text>
+        <Text style={styles.title2}>YOUR MEALS</Text>
         <TextInput
           style={styles.input}
           value={meals}
@@ -52,8 +52,10 @@ export default function Services() {
         />
       </View>
 
-      <Text style={styles.title}>YOUR MOVIES</Text>
-      <ListInput movies={dataMovies} />
+      <View style={styles.movie}>
+        <Text style={styles.title}>YOUR MOVIES</Text>
+        <ListInput movies={dataMovies} />
+      </View>
     </View>
   );
 }
@@ -62,6 +64,7 @@ const styles = StyleSheet.create({
   onglet: {
     backgroundColor: '#75bbf4',
     flex: 1,
+    alignItems: 'center',
   },
   title: {
     fontFamily: 'Cabin-Bold',
@@ -69,19 +72,35 @@ const styles = StyleSheet.create({
     paddingLeft: 20,
     paddingTop: 10,
   },
+  title2: {
+    fontFamily: 'Cabin-Bold',
+    color: '#002C82',
+    paddingLeft: 20,
+  },
   input: {
-    flex: 1,
-    width: 350,
-    height: 10,
+    
+    width: 190,
+    height: 30,
     borderWidth: 1,
     borderColor: '#002C82',
     borderRadius: 6,
-
+   
     margin: 10,
   },
-  otherService:{
-    flexDirection:'row',
-    alignItems:'center',
-    backgroundColor:'pink'
-  }
+  otherService: {
+    width: '95%',
+    height: '20%',
+    flexDirection: 'row',
+    alignItems: 'center',
+ 
+    borderBottomWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#002C82',
+
+  },
+  movie: {
+    width: '95%',
+    height: '60%',
+
+  },
 });
