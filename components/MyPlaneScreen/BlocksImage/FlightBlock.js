4ced34b948fe2a869d5cb9442a37d4c490846e7d
Anthony <anthonyiezzi64@gmail.com>
2024-02-23 10:55:55 +0100

    marker ok

--- components/MyPlaneScreen/BlocksImage/FlightBlock.js
+++ components/MyPlaneScreen/BlocksImage/FlightBlock.js
@@ -1,8 +1,78 @@
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
-import React from 'react';
-import MapView, {Marker} from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+//Redux
+import { useSelector } from 'react-redux';
+
+//Local address
+const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 
 export default function FlightBlock() {
+  //Utilisation du Redux
+  const flightRedux = useSelector((state) => state.flights.value);
+  console.log('flightRedux in service :',flightRedux);
+
+  // State local pour stocker les coordonnées des aéroports
+  const [mapFlight, setMapFlight] = useState({
+    latA: 0,
+    latD: 0,
+    longA: 0,
+    longD: 0,
+  });
+
+  //useEffect pour charger les markers au lancement du screen
+  useEffect(() => {
+    handleFetchDataIataDep();
+    handleFetchDataIataArr();
+  }, []);
+
+  //Fetch des long et lat du flight en fonction des iataCode
+  const handleFetchDataIataArr = async () => {
+    try {
+      const response = await fetch(
+        `${apiUrl}/flights/map/${flightRedux[0].iataArrival}`
+      );
+      const iataDataA = await response.json();
+
+      if (iataDataA.result) {
+        console.log(
+          `IataDataArr: lat is ${iataDataA.data.latitude} and long is ${iataDataA.data.longitude}`
+        );
+       setMapFlight(prevState => ({
+          ...prevState,
+          latA: iataDataA.data.latitude,
+          longA: iataDataA.data.longitude
+        }));
+      } else {
+        console.error('Error during connection', iataDataA.error);
+      }
+    } catch (error) {
+      console.error('Error during connection:', error);
+    }
+  };
+  const handleFetchDataIataDep = async () => {
+    try {
+      const response = await fetch(
+        `${apiUrl}/flights/map/${flightRedux[0].iataDep}`
+      );
+      const iataDataD = await response.json();
+
+      if (iataDataD.result) {
+        console.log(
+          `IataDataADep: lat is ${iataDataD.data.latitude} and long is ${iataDataD.data.longitude}`
+        );
+        setMapFlight(prevState => ({
+          ...prevState,
+          latD: iataDataD.data.latitude,
+          longD: iataDataD.data.longitude
+        }));
+      } else {
+        console.error('Error during connection', iataDataD.error);
+      }
+    } catch (error) {
+      console.error('Error during connection:', error);
+    }
+  };
   return (
     <View style={styles.mapContainer}>
       <MapView
@@ -14,10 +84,8 @@ export default function FlightBlock() {
           longitudeDelta: 50,
         }}
       >
-        <Marker
-        coordinate={{latitude:37.7, longitude:-122.49}}/>
-        <Marker
-        coordinate={{latitude:38.7, longitude:-110.49}}/>
+        <Marker coordinate={{ latitude: mapFlight.latD, longitude: mapFlight.longD }} />
+        <Marker coordinate={{ latitude: mapFlight.latA, longitude: mapFlight.longA }} />
       </MapView>
     </View>
   );
