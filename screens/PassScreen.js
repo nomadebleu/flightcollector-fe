58a9cffa7c8312f0cbe408417f7a0fe3ffb196f3
Vanessa <vane.giraldo14@gmail.com>
2024-02-26 18:06:01 +0100

    map distance line & lat-long delta modification

--- screens/PassScreen.js
+++ screens/PassScreen.js
@@ -72,6 +72,7 @@ export default function PassScreen() {
           console.log("FlightData:", flightData.data);
           dispatch(
             addFlight({
+              _id: flightData.data._id,
               planes: flightData.data.planes,
 
               departure: flightData.data.departure,
